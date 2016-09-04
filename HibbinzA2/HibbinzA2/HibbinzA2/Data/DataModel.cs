fb1aadb9f224470dff00b6651d57e50a420b23c6
mirrin <rhibbins@deakin.edu.au>
2016-09-04 18:05:13 +1000

    Trying to add Data base without debug !!  basic concepts data model .. cell page and list page

---
+++ HibbinzA2/HibbinzA2/HibbinzA2/Data/DataModel.cs
@@ -0,0 +1,19 @@
+﻿namespace HibbinzA2.Data
+{
+    public class ShopTrack
+    {
+        public ShopTrack()
+        {
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Notes { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
+
---
+++ HibbinzA2/HibbinzA2/HibbinzA2/Data/ShoptrackDataBase.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HibbinzA2.Data
+{
+    public class ShoptrackDataBase
+    {
+        static object locker = new object();
+
+        SQLiteConnection database;
+
+        string DatabasePath
+        {
+            get
+            {
+                var sqliteFilename = "ShopTrack.db3";
+
+#if __ANDROID__
+				string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+				var path = Path.Combine(documentsPath, sqliteFilename);
+
+#endif
+                return Path;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tasky.DL.TaskDatabase"/> TaskDatabase. 
+        /// if the database doesn't exist, it will create the database and all the tables.
+        /// </summary>
+        /// <param name='path'>
+        /// Path.
+        /// </param>
+        public ShoptrackDataBase()
+        {
+            database = new SQLiteConnection(DatabasePath);
+            // create the tables
+            database.CreateTable<ShopTrack>();
+        }
+
+        public IEnumerable<ShopTrack> GetItems()
+        {
+            lock (locker)
+            {
+                return (from i in database.Table<ShopTrack>() select i).ToList();
+            }
+        }
+
+        public IEnumerable<ShopTrack> GetItemsNotDone()
+        {
+            lock (locker)
+            {
+                return database.Query<ShopTrack>("SELECT * FROM [ShopTrack] WHERE [Done] = 0");
+            }
+        }
+
+        public ShopTrack GetItem(int id)
+        {
+            lock (locker)
+            {
+                return database.Table<ShopTrack>().FirstOrDefault(x => x.ID == id);
+            }
+        }
+
+        public int SaveItem(ShopTrack item)
+        {
+            lock (locker)
+            {
+                if (item.ID != 0)
+                {
+                    database.Update(item);
+                    return item.ID;
+                }
+                else
+                {
+                    return database.Insert(item);
+                }
+            }
+        }
+
+        public int DeleteItem(int id)
+        {
+            lock (locker)
+            {
+                return database.Delete<ShopTrack>(id);
+            }
+        }
+    }
+}
+
